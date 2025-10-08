4d8b38c3f729198f4d657f6207ef216944d55d48
Codfacile <andrea.verde.1999@gmail.com>
2025-10-08 17:29:47 +0200

    new port forwarding for api persona

--- actions/actionsUpdateFromFile.ts
+++ actions/actionsUpdateFromFile.ts
@@ -221,7 +221,7 @@ export async function importaPersone(personeInput: PersonaInput[]) {
 
     const response = await fetch(
       // "https://worker-gestionale-recupero-crediti-ewt7.onrender.com/anagrafica",
-      "http://db1.ddns.net:32777/anagrafica",
+      "http://db1.ddns.net:5052/anagrafica",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
