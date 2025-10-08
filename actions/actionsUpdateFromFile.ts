c2229f9385d4dcdc22e3af019f159ecbfcbd7b6c
Codfacile <andrea.verde.1999@gmail.com>
2025-10-08 19:46:55 +0200

    fix port persona temp

--- actions/actionsUpdateFromFile.ts
+++ actions/actionsUpdateFromFile.ts
@@ -221,7 +221,7 @@ export async function importaPersone(personeInput: PersonaInput[]) {
 
     const response = await fetch(
       // "https://worker-gestionale-recupero-crediti-ewt7.onrender.com/anagrafica",
-      "http://db1.ddns.net:32781/anagrafica",
+      "http://db1.ddns.net:32782/anagrafica",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
